9bc28529ce93914b51fca4e814db1dbd2b50eb65
Pinebanana <101684591+Pinebanana@users.noreply.github.com>
2022-03-22 18:42:40 +0900

    Add files via upload

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -40,22 +40,25 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-ETC")
         end_time = start_time + datetime.timedelta(days=1)
 
-        # 9:00 < 현재 < #08:59:59
+        # 9:00 < 현재 < #08:59:50
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            current_price = get_current_price("KRW-BTC")
+            target_price = get_target_price("KRW-ETC", 0.4)
+            current_price = get_current_price("KRW-ETC")
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    upbit.buy_market_order("KRW-ETC", krw*0.9995)
         else:
-            btc = get_balance("BTC")
-            if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+            ETC = get_balance("ETC")
+            if ETC > 0.00008:
+                upbit.sell_market_order("KRW-ETC", ETC*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
         time.sleep(1)
+
+
+        
